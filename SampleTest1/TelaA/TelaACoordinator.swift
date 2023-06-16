65f6a0c28c8959dfd3db4ede45bfac977e2ddb97
Douglas  Rodrigues <douglasrodrigues@Mac-mini-de-Douglas.local>
2023-06-16 15:01:55 -0300

    adicionar coordinators

---
+++ SampleTest1/TelaA/TelaACoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  TelaACoordinator.swift
+//  SampleTest1
+//
+//  Created by Douglas  Rodrigues  on 16/06/23.
+//
+
+import UIKit
+
+final class TelaACoordinator: Coordinator {
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewModel = TelaAViewModel()
+        let viewController = TelaAViewController(viewModel: viewModel)
+        viewModel.coordinator = self
+        navigationController.setViewControllers([viewController], animated: false)
+    }
+    
+    func startTelaB(parameter: String) {
+        let telaBCoordinator = TelaBCoordinator(navigationController: navigationController,
+                                                parameter: parameter)
+        childCoordinators.append(telaBCoordinator)
+        telaBCoordinator.start()
+    }
+}
